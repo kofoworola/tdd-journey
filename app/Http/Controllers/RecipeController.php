93de7c7f0249f7ff654edc19500657fd63eb00da
Kofo Okesola <okesolakofo@gmail.com>
2019-01-17 10:13:13 +0100

    Added recipe controller and test

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+    public function create(Request $request){
+        $this->validate($request,['title' => 'required','procedure' => 'required|min:100']);
+    }
 }
